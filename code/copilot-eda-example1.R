b7a806f99764d8064622a6080864279e94902f8c
Andreas Handel <andreas.handel@gmail.com>
2023-12-28 11:44:55 -0500

    more

--- code/copilot-eda-example1.R
+++ code/copilot-eda-example1.R
@@ -1,6 +1,6 @@
 # write code that loads data from a file
 # the data is stored in the dat object
-# the code should produces several plots of the variables stored in the dat object
+# the code should produce several plots of the variables stored in the dat object
 
 library(here)
 library(tidyverse)
@@ -10,4 +10,9 @@ dat <- read.csv(here::here("data","syn_dat.csv"))
 
 # make a summary table of the data stored in the dat object
 summary(dat)
+
+# make a histogram of the Age variable
+ggplot(dat, aes(x = Age)) + geom_histogram()
+
+# make a scatter plot of BloodPressure versus Age, stratified by Gender
 
