1c9afedf2cfe1a160cd0492b4adcc17a211d078e
Andreas Handel <andreas.handel@gmail.com>
2024-03-07 10:16:18 -0500

    updates

--- myresources/fitting-exercise-solution/fitting-exercise.R
+++ myresources/fitting-exercise-solution/fitting-exercise.R
@@ -4,10 +4,8 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 
-
 ## ---- load-data --------
 rawdat <- readr::read_csv("Mavoglurant_A2121_nmpk.csv")
-
 
 ## ---- explore-data --------
 summary(rawdat)
@@ -16,7 +14,6 @@ p1 <- rawdat %>% ggplot() +
   geom_line( aes( x = TIME, y = DV, group = as.factor(ID), color = as.factor(DOSE)) ) +
   facet_wrap( ~ DOSE, scales = "free_y")
 plot(p1)
-
 
 ## ---- process-data --------
 # remove those with OCC = 2
@@ -38,49 +35,63 @@ dat_merge <- left_join(dat_y, dat_t0)
 dat <- dat_merge %>% select(Y,DOSE,AGE,SEX,RACE,WT,HT) %>% mutate_at(vars(SEX,RACE), factor) 
 readr::write_rds(dat,"mavoglurant.rds")
 
-## ---- fit-data-linear --------
+
+# fit the linear models with Y as outcome 
+# first model has only DOSE as predictor
+# second model has all variables as predictors
 lin_mod <- linear_reg() %>% set_engine("lm")
 linfit1 <- lin_mod %>% fit(Y ~ DOSE, data = dat)
 linfit2 <- lin_mod %>% fit(Y ~ ., data = dat)
-# Compute the RMSE and R squared for each model
+
+# Compute the RMSE and R squared for model 1
 metrics_1 <- linfit1 %>% 
   predict(dat) %>% 
   bind_cols(dat) %>% 
   metrics(truth = Y, estimate = .pred)
+
+# Compute the RMSE and R squared for model 2
 metrics_2 <- linfit2 %>% 
   predict(dat) %>% 
   bind_cols(dat) %>% 
   metrics(truth = Y, estimate = .pred)
+
 # Print the results
 print(metrics_1)
 print(metrics_2)
 
 
 ## ---- fit-data-logistic --------
+# fit the logistic models with SEX as outcome 
+# first model has only DOSE as predictor
+# second model has all variables as predictors
 log_mod <- logistic_reg() %>% set_engine("glm")
 logfit1 <- log_mod %>% fit(SEX ~ DOSE, data = dat)
 logfit2 <- log_mod %>% fit(SEX ~ ., data = dat)
-# Compute the accuracy and AUC for each model
+
+# Compute the accuracy and AUC for model 1
 m1_acc <- logfit1 %>% 
   predict(dat) %>% 
   bind_cols(dat) %>% 
   metrics(truth = SEX, estimate = .pred_class) %>% 
   filter(.metric == "accuracy") 
 m1_auc <-  logfit1 %>%
-      predict(dat, type = "prob") %>%
-      bind_cols(dat) %>%
-      roc_auc(truth = SEX, .pred_1)
+  predict(dat, type = "prob") %>%
+  bind_cols(dat) %>%
+  roc_auc(truth = SEX, .pred_1)
+
+# Compute the accuracy and AUC for model 2
 m2_acc <- logfit2 %>% 
   predict(dat) %>% 
   bind_cols(dat) %>% 
   metrics(truth = SEX, estimate = .pred_class) %>% 
-  filter(.metric %in% c("accuracy", "roc_auc"))
+  filter(.metric %in% c("accuracy"))
 m2_auc <-  logfit2 %>%
   predict(dat, type = "prob") %>%
   bind_cols(dat) %>%
   roc_auc(truth = SEX, .pred_1)
+
 # Print the results
 print(m1_acc)
-print(m1_auc)
 print(m2_acc)
+print(m1_auc)
 print(m2_auc)
--- myresources/improvement-exercise-solution/improvement-exercise.R
+++ myresources/improvement-exercise-solution/improvement-exercise.R
@@ -4,12 +4,9 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 
-
 ## ---- load-data --------
 dat <- readr::read_rds("mavoglurant.rds")
 set.seed(1234)
-
-
 
 ## ---- split-data --------
 # Put 3/4 of the data into the training set 
@@ -20,72 +17,26 @@ train_data <- training(data_split)
 test_data  <- testing(data_split)
 
 
-## ---- fit-data-linear --------
+
 mod <- linear_reg() %>% set_engine("lm")
 wflow <- 
   workflow() %>% 
   add_model(mod) %>% 
   add_formula(Y ~ DOSE)
-
 fit1 <- wflow %>% fit(data = train_data)
 
+# get predictions for both train and test data
 pred_train <- predict(fit1, train_data)
 pred_test <- predict(fit1, test_data)
 
-
+# compute RMSE for train data
 rmse_train <- pred_train %>% 
   bind_cols(train_data) %>% 
   metrics(truth = Y, estimate = .pred)
 print(rmse_train)
 
+# compute of RMSE for test data
 rmse_test <- pred_test %>% 
   bind_cols(test_data) %>% 
   metrics(truth = Y, estimate = .pred)
 print(rmse_test)
-
-
-
-lin_mod <- linear_reg() %>% set_engine("lm")
-linfit1 <- lin_mod %>% fit(Y ~ DOSE, data = dat)
-linfit2 <- lin_mod %>% fit(Y ~ ., data = dat)
-# Compute the RMSE and R squared for each model
-metrics_1 <- linfit1 %>% 
-  predict(dat) %>% 
-  bind_cols(dat) %>% 
-  metrics(truth = Y, estimate = .pred)
-metrics_2 <- linfit2 %>% 
-  predict(dat) %>% 
-  bind_cols(dat) %>% 
-  metrics(truth = Y, estimate = .pred)
-# Print the results
-print(metrics_1)
-print(metrics_2)
-
-
-## ---- fit-data-logistic --------
-logfit1 <- log_mod %>% fit(SEX ~ DOSE, data = dat)
-logfit2 <- log_mod %>% fit(SEX ~ ., data = dat)
-# Compute the accuracy and AUC for each model
-m1_acc <- logfit1 %>% 
-  predict(dat) %>% 
-  bind_cols(dat) %>% 
-  metrics(truth = SEX, estimate = .pred_class) %>% 
-  filter(.metric == "accuracy") 
-m1_auc <-  logfit1 %>%
-      predict(dat, type = "prob") %>%
-      bind_cols(dat) %>%
-      roc_auc(truth = SEX, .pred_1)
-m2_acc <- logfit2 %>% 
-  predict(dat) %>% 
-  bind_cols(dat) %>% 
-  metrics(truth = SEX, estimate = .pred_class) %>% 
-  filter(.metric %in% c("accuracy"))
-m2_auc <-  logfit2 %>%
-  predict(dat, type = "prob") %>%
-  bind_cols(dat) %>%
-  roc_auc(truth = SEX, .pred_1)
-# Print the results
-print(m1_acc)
-print(m1_auc)
-print(m2_acc)
-print(m2_auc)
